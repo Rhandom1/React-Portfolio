2b79cbfb09b1abda8ced0f9cbc6be1d235780996
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-12 15:47:36 -0600

    About page built with images

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,24 +3,28 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
-        Welcome! I am a full-stack developer looking to break into the field of Educational Software development.
-        After spending several years as a veterinary technician, I found my calling in teaching theatre to middle school students.
-        After losing the funding for my teaching position, I researched ways I could use my IT skills in a new way to continue pursuing my passion of educating our future.
-        I have found my next step in development, and I really enjoy the creative front-end work as well as teh more logic driven back-end development.
-      </p>
-      <p> 
-        Professionally, I have several years of customer service and leadership skills. I am an excellent communicator and collaborator
-        while equally comfortable working independently. I have built empathetic listening skills through multiple leadership positions
-        and clinical work. I am an agile problem sover and unafraid to make mistakes or admit when I need help.
-        My Google skills are growing rapidly!
-      </p>
-      <p>
-        In my personal life, I am devoted father to a beautiful son named Ronan. I love to travel and look forward to being able to show my son the world.
-        I love reading, creating stories with my son, and gaming. Thursdays are family Roblox nights so you can often find me there
-        playing with my son and wife.
-      </p>
+      <article>
+        <h1>About Me</h1>
+        <img class="BooBash" src="../../Assets/Disboobash19(2).jpeg" alt="My family at Disney"></img>
+        <p>
+          Welcome! I am a full-stack developer looking to break into the field of Educational Software development.
+          After spending several years as a veterinary technician, I found my calling in teaching theatre to middle school students.
+          After losing the funding for my teaching position, I researched ways I could use my IT skills in a new way to continue pursuing my passion of educating our future.
+          I have found my next step in development, and I really enjoy the creative front-end work as well as teh more logic driven back-end development.
+        </p>
+        <p> 
+          Professionally, I have several years of customer service and leadership skills. I am an excellent communicator and collaborator
+          while equally comfortable working independently. I have built empathetic listening skills through multiple leadership positions
+          and clinical work. I am an agile problem sover and unafraid to make mistakes or admit when I need help.
+          My Google skills are growing rapidly!
+        </p>
+        <p>
+          In my personal life, I am devoted father to a beautiful son named Ronan. I love to travel and look forward to being able to show my son the world.
+          I love reading, creating stories with my son, and gaming. Thursdays are family Roblox nights so you can often find me there
+          playing with my son and wife.
+        </p>
+        <img class="BooBash" src="../../Assets/Fazbear21.jpg" alt="Halloween bear 2021"></img>
+      </article>
     </div>
   );
 }
