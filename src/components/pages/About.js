65a5f2c4b10d45fb29cb897db445e58ba1ded02f
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-12 15:36:01 -0600

    Building About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,12 +5,21 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Welcome! I am a full-stack developer looking to break into the field of Educational Software development.
+        After spending several years as a veterinary technician, I found my calling in teaching theatre to middle school students.
+        After losing the funding for my teaching position, I researched ways I could use my IT skills in a new way to continue pursuing my passion of educating our future.
+        I have found my next step in development, and I really enjoy the creative front-end work as well as teh more logic driven back-end development.
+      </p>
+      <p> 
+        Professionally, I have several years of customer service and leadership skills. I am an excellent communicator and collaborator
+        while equally comfortable working independently. I have built empathetic listening skills through multiple leadership positions
+        and clinical work. I am an agile problem sover and unafraid to make mistakes or admit when I need help.
+        My Google skills are growing rapidly!
+      </p>
+      <p>
+        In my personal life, I am devoted father to a beautiful son named Ronan. I love to travel and look forward to being able to show my son the world.
+        I love reading, creating stories with my son, and gaming. Thursdays are family Roblox nights so you can often find me there
+        playing with my son and wife.
       </p>
     </div>
   );
