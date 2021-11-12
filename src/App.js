d01f57de16a7bc86de6387a9d1c5d8ecf7f9f9b3
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-12 10:06:18 -0600

    Initial commit with npx files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <PortfolioPages />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PortfolioPages.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioPages() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    // Set up const to conditionally render the pages
+    const renderPage = () => {
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+        if (currentPage === 'Projects') {
+            return <Projects />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        return <About />;
+    };
+
+    // Add page change and set variable
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+    );
+}
