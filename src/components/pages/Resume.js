eb745551672408a3cca07766790441033cb7e6b7
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-12 23:37:04 -0600

    Link for resume added

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Resume() {
+    <a
+              href="https://docs.google.com/document/d/12JgnmyeI3cITsUCkqEAPcf4V13wS5-sA/edit?usp=sharing&ouid=105410131232769335027&rtpof=true&sd=true"
+              target="_blank"> Click here to view my resumeResume</a>
+}
+
+//List of tech and soft skills
