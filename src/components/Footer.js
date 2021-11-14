713981fe19960b458db4f0b35086741fc888f8c2
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-14 14:49:23 -0600

    Added/fixed icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
     return (
