21e3aacf9c562e5bac2a2c2cb232b349316d23e4
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-14 15:35:16 -0600

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,19 @@ import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
     return (
-        <div>
-            <a href="https://github.com/Rhandom1" rel="noreferrer"  target="_blank"><i className="fab fa-github fa-2x social"></i></a>
-            <a href="https://www.linkedin.com/in/shawn-mcwhorter-a4b12170/" rel="noreferrer" target="_blank"><i className="fab fa-linkedin fa-2x social"></i></a>
-            <a href="https://www.instagram.com/rhandom01/" rel="noreferrer" target="_blank"><i className="fab fa-instagram fa-2x social"></i></a>
-        </div>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top footer">
+            <div className="col-md-4 d-flex align-items-center">
+                <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <svg className="bi" width="30" height="24"></svg>
+                </a>
+                <span className="text-muted">© 2021 Shawn McWhorter</span>
+            </div>
+            <div className="nav col-md-4 justify-content-start list-unstyled d-flex">
+                <a style={{ marginRight: 10 }} href="https://github.com/Rhandom1"><FaGithub size='50px' /></a>
+                <a style={{ marginRight: 10 }} href="https://www.linkedin.com/in/shawn-mcwhorter/" ><FaLinkedin size='50px' /></a>
+                <a style={{ marginRight: 10 }} href="https://www.instagram.com/rhandom01/" ><FaInstagram size='50px' /></a>
+            </div>
+        </footer>
     )
 }
 
