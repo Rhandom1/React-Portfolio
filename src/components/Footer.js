e1c94000666d9d2b92f32a402a61734871ad195b
Shawn McWhorter <73659834+Rhandom1@users.noreply.github.com>
2021-11-15 22:52:51 -0600

    Attempting to fix footer positioning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,10 @@ import { FaInstagram } from "react-icons/fa";
 function Footer() {
     return (
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top footer">
-            <div className="col-md-4 d-flex align-items-center">
-                <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                <svg className="bi" width="30" height="24"></svg>
-                </a>
-                <span className="text-muted">© 2021 Shawn McWhorter</span>
-            </div>
-            <div className="nav col-md-4 justify-content-start list-unstyled d-flex">
-                <a style={{ marginRight: 10 }} href="https://github.com/Rhandom1"><FaGithub size='50px' /></a>
-                <a style={{ marginRight: 10 }} href="https://www.linkedin.com/in/shawn-mcwhorter/" ><FaLinkedin size='50px' /></a>
-                <a style={{ marginRight: 10 }} href="https://www.instagram.com/rhandom01/" ><FaInstagram size='50px' /></a>
-            </div>
+                <a style={{ margin: 0 }} href="https://github.com/Rhandom1"><FaGithub size='50px' /></a>
+                <a style={{ margin: 0  }} href="https://www.linkedin.com/in/shawn-mcwhorter/" ><FaLinkedin size='50px' /></a>
+                <a style={{ margin: 0  }} href="https://www.instagram.com/rhandom01/" ><FaInstagram size='50px' /></a>
+            
         </footer>
     )
 }
